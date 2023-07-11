25a04c41ce724dea2124f80e4272d06f59a36653
Prerna <iprernasingh4@gmail.com>
2023-07-11 19:05:27 +0530

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const addButton = document.querySelector("#add");
 
+const updateLSData = () => {
+  const textAreaData = document.querySelectorAll("textarea");
+  const notes = [];
+
+  textAreaData.forEach((note) => {
+    return notes.push(note.value);
+  });
+
+  localStorage.setItem("notes", JSON.stringify(notes));
+};
+
 const addNewNote = (text = "") => {
   const note = document.createElement("div");
   note.classList.add("note");
@@ -10,11 +21,47 @@ const addNewNote = (text = "") => {
           <button class="delete"><i class='bx bxs-trash'></i></button>
        </div>
       
-      <div class="main"></div>
-      <textarea class=""></textarea>   `;
+      <div class="main ${text ? "" : "hidden"}"> </div>
+      <textarea class="${text ? "hidden" : ""}"></textarea> `;
 
   note.insertAdjacentHTML("afterbegin", htmlData);
   //console.log("note");
+
+  //getting the refrences
+  const editButton = note.querySelector(".edit");
+  const delButton = note.querySelector(".delete");
+  const mainDiv = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
+
+  //deleting the node
+  delButton.addEventListener("click", () => {
+    note.remove();
+    updateLSData();
+  });
+
+  //toggle using edit button
+  textArea.value = text;
+  mainDiv.innerHTML = text;
+
+  editButton.addEventListener("click", () => {
+    mainDiv.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
+
+  textArea.addEventListener("change", (event) => {
+    const value = event.target.value;
+    mainDiv.innerHTML = value;
+
+    updateLSData();
+  });
+
+  document.body.appendChild(note);
 };
 
+//getting data back from localStorage
+const notes = JSON.parse(localStorage.getItem("notes"));
+
+if (notes) {
+  notes.forEach((note) => addNewNote(note));
+}
 addButton.addEventListener("click", () => addNewNote());
